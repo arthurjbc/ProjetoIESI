d165b335c4be388fad5e2434a5b7dde5211c5875
Romero <romerofilho03@gmail.com>
2025-12-15 12:01:41 -0300

    refactor(main): created new client and service folders. Moved all python backend for services

--- main.py
+++
@@ -1,5 +0,0 @@
-from geradorJSON import gerar_json
-from geradorZPL import root
-
-gerar_json()
-root.mainloop()
---
+++ server/main.py
@@ -0,0 +1,5 @@
+from server.geradorJSON import gerar_json
+from server.geradorZPL import root
+
+gerar_json()
+root.mainloop()
